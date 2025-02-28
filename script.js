9680cd5bff1f5c7006e7cf5e82f3a1a81bd9f9bf
Kirsty Ratchford <Kirsty.Ratchford@hotmail.com>
2025-02-28 11:48:45 +0000

    slight amendment to js

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ document.querySelector('form').addEventListener('submit', function(event) {
     alert('Message Sent! I will get back to you soon.');
 });
 
-document.querySelector(".navbar-toggler").addEventListener("click", function () {
-    let expanded = this.getAttribute("aria-expanded") === "true";
-    this.setAttribute("aria-expanded", !expanded);
-});
-
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
     this.reset();
